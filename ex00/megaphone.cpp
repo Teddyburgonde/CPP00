d33dd8f990bcc0bb2a24c3b16afe3344075868cc
Teddyburgonde <bandama.teddy@gmail.com>
2024-07-18 16:49:11 +0200

    to do logique for copy

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,6 +1,32 @@
-#include <iostream>
 
-int main() {
-    std::cout << "hello world !\n";
-    return (0);
+#include <iostream> // Permet d'utiliser le cout 
+#include <string> // Permet d'utiliser std::string
+#include <cctype> // Permet d'utiliser islower et toupper
+
+int main(int argc, char **argv) {
+    std::string str;
+    // Si il y a pas le bon nombres d'arguments
+    if (argc < 2) {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOIS *\n";
+        return (1);
+    }
+    // copie des valeurs
+    str = argv[1];
+    
+    toupper(str);
+    std::cout << str;
+    // // permet d'ajouter le '\n'
+    // std::cout << std::endl;
 }
+
+
+
+
+
+
+
+
+
+
+
+
