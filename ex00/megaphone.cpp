ed9aaa8582bbb06fbbab0f2cc102fe69a581a377
Teddyburgonde <bandama.teddy@gmail.com>
2024-07-18 21:44:01 +0200

    to do norme

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -4,22 +4,26 @@
 #include <cctype> // Permet d'utiliser islower et toupper
 
 int main(int argc, char **argv) {
+    // declaration de la string
     std::string str;
+
     // Si il y a pas le bon nombres d'arguments
     if (argc < 2) {
         std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOIS *\n";
         return (1);
     }
     // copie des valeurs
-    str = argv[1];
-    
-    toupper(str);
-    std::cout << str;
-    // // permet d'ajouter le '\n'
-    // std::cout << std::endl;
+    for(int i = 1; argv[i]; i++) {
+        str += argv[i];
+    }
+    for(int i = 0; str[i]; i++) {
+        // Transforme le caractere en majuscule
+        // imprime le contenu 
+        std::cout << (char)toupper(str[i]);
+    }
+    //permet d'ajouter le '\n'
+    std::cout << std::endl;
 }
-
-
 
 
 
