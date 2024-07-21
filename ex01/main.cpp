a571b6486051ad516c8d050bada6bcbb2d5a07df
Teddyburgonde <bandama.teddy@gmail.com>
2024-07-21 23:12:54 +0200

    added pseudo code on commentary

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,10 +3,12 @@
 // 1. Cree une classe PhoneBook //! Finish 
 // 2. Cree phone qui est une instance de la classe PhoneBook //! Finish
 // 3. Crer un tableau de contact de la taille de 8 max. //! Finish 
-// 4. Ajouter une option pour que l'utilisateur peut rajouter un contact.
-// 5. Pouvoir remplacer le plus ancien par le dernier contact ajouter.
-// 6. Ajouté une classe privé pour ce qui est dans une classe
-// 7. Ajouté une classe public pour ce qui est a l'exterieur de la classe
+// 4. Comment initialiser attributs tableau de contact
+
+// 5. Ajouter une option pour que l'utilisateur peut rajouter un contact.
+// 6. Pouvoir remplacer le plus ancien par le dernier contact ajouter.
+// 7. Ajouté une classe privé pour ce qui est dans une classe
+// 8. Ajouté une classe public pour ce qui est a l'exterieur de la classe
 
 #include "PhoneBook.class.hpp"
 
